edddf857e1cdd961d3cae63b6715f2a68cf3e677
Natalia Podosinnikova <Natalia@Natalias-MacBook-Air.local>
2016-02-18 17:12:29 -0600

    finished

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ document.querySelector("#thanks button").addEventListener('click',function(){
 document.querySelector("#double button").addEventListener('click',function(){
   // TASK #2
   var text = document.querySelector('#compoundInvestment')
-  console.log("text")
   text.innerHTML = text.innerHTML * 2
 })
 
@@ -36,20 +35,80 @@ document.querySelector("#blow-up button").addEventListener('click',function(){
        redCirc.style.height = '40px'
      }
 })
-
+var inactiveShowing = true
 document.querySelector("#remove button").addEventListener('click',function(){
   // TASK #5
+  var userList = document.querySelector("ul#userList")
+  var inactiveUsers = userList.querySelectorAll("li.inactive") 
+    for (var i = 0; i < inactiveUsers.length; i++) {
+    //userList.removeChild(inactiveUsers[i])
+    if (inactiveShowing) { 
+      inactiveUsers[i].style.display = 'none'
+    }
+    else {
+      inactiveUsers[i].style.display = 'list-item'
+
+      }  
+  } 
+  if(inactiveShowing) inactiveShowing = false
+  else inactiveShowing = true
 })
 
 
 document.querySelector("#reverse-squares button").addEventListener('click',function(){
   // TASK #6
-})
+    var reverseSquares = function (inputArray) {
+      var newArray = []
+      for (var i = inputArray.length - 1; i >=0; i--) {
+        newArray.push(inputArray[i])
+      }
+      return newArray
+    }
+
+  var squares = document.querySelectorAll(".square")
+  
+  var reversedSquares = reverseSquares(squares)
+
+  var answerBox = document.querySelector("#reverse-squares .answer-box")
+  answerBox.innerHTML = ""
+  //console.log([answerBox]) //<= brackets give details of DOM object
+  for (var i = 0; i < reversedSquares.length; i++) {
+    answerBox.appendChild(reversedSquares[i])
+    
+    // alternatively ...
+    // answerBox.innerHTML += reversedSquares[i].outerHTML
+  }
+
+ })
 
 document.querySelector("#pig-latin button").addEventListener('click',function(){
   // TASK #7
+
+  var reverseString = function(inputString) {
+  var outputString = ""
+  for (var i = inputString.length -1; i >=0; i--) {
+        outputString += inputString[i]
+      }
+      return outputString
+  }
+
+  var taskList = document.querySelector("ul#tasks")
+  var tasksCollection = taskList.querySelectorAll("li")
+    for (var i = 0; i < tasksCollection.length; i++) {  
+    tasksCollection[i].textContent = reverseString(tasksCollection[i].textContent)
+    //tasksCollection[i].textContent = tasksCollection[i].textContent.reverse()
+  }
 })
 
 document.querySelector("#cycle-image button").addEventListener('click',function(){
   // TASK #8
+  
+  var cityImgEl = document.querySelector("#city-img")
+  var url = cityImgEl.src 
+  var urlParts = url.split("/")
+  var photoId = urlParts.pop()
+  var newId = parseInt(photoId) + 1
+  newId = ((newId - 1) % 10) + 1 // <= cycle up to 10, reset to 1
+  urlParts.push(newId)
+  cityImgEl.src = urlParts.join("/")
 })
